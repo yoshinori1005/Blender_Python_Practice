4ab14708183f34e43de5b35b850b9b55c0370698
yoshinori1005 <s-yoshinori@jcom.home.ne.jp>
2025-03-03 14:00:44 +0900

    ノイズマスク作成
    粗さにノイズマスクを作成するノード群を追加

--- create_simple_material.py
+++ create_simple_material.py
@@ -1,6 +1,9 @@
 # PythonにBlenderの機能へのアクセスを許可する
 import bpy
 
+# Python の数学機能を拡張する
+import math
+
 # 乱数を生成するための Python 機能を拡張する
 import random
 
@@ -20,6 +23,74 @@ def partially_clean_the_scene():
     )
 
 
+# ノイズ マスクを作成する関数
+def create_noise_mask(material):
+    """次のノードを使用して、ノイズ マスクを作成するためのノード セットを追加する
+    * テクスチャ座標ノード
+    * マッピングノード
+    * ノイズテクスチャノード
+    * カラーランプノード
+    """
+
+    node_location_x_step = 300
+    node_location_x = -node_location_x_step
+
+    # カラーランプノードの作成
+    # https://docs.blender.org/api/current/bpy.types.ShaderNodeValToRGB.html
+    color_ramp_node = material.node_tree.nodes.new(type="ShaderNodeValToRGB")
+    color_ramp_node.color_ramp.elements[0].position = 0.45
+    color_ramp_node.color_ramp.elements[1].position = 0.5
+    color_ramp_node.location.x = node_location_x
+    node_location_x -= node_location_x_step
+
+    # ノイズテクスチャノードの作成
+    # https://docs.blender.org/api/current/bpy.types.ShaderNodeTexNoise.html#bpy.types.ShaderNodeTexNoise
+    noise_texture_node = material.node_tree.nodes.new(type="ShaderNodeTexNoise")
+    noise_texture_node.inputs["Scale"].default_value = random.uniform(1.0, 20.0)
+    noise_texture_node.location.x = node_location_x
+    node_location_x -= node_location_x_step
+
+    # マッピングノードの作成
+    # https://docs.blender.org/api/current/bpy.types.ShaderNodeMapping.html#bpy.types.ShaderNodeMapping
+    mapping_node = material.node_tree.nodes.new(type="ShaderNodeMapping")
+    mapping_node.inputs["Rotation"].default_value.x = math.radians(
+        random.uniform(0.0, 360.0)
+    )
+    mapping_node.inputs["Rotation"].default_value.y = math.radians(
+        random.uniform(0.0, 360.0)
+    )
+    mapping_node.inputs["Rotation"].default_value.z = math.radians(
+        random.uniform(0.0, 360.0)
+    )
+    mapping_node.location.x = node_location_x
+    node_location_x -= node_location_x_step
+
+    # テクスチャ座標ノードの作成
+    texture_coordinate_node = material.node_tree.nodes.new(type="ShaderNodeTexCoord")
+    texture_coordinate_node.location.x = node_location_x
+    node_location_x -= node_location_x_step
+
+    # ノードを接続
+    # https://docs.blender.org/api/current/bpy.types.NodeTree.html#bpy.types.NodeTree
+    # https://docs.blender.org/api/current/bpy.types.NodeLinks.html#bpy.types.NodeLinks
+
+    # ノイズテクスチャノードからカラーランプノードへの接続
+    material.node_tree.links.new(
+        noise_texture_node.outputs["Color"], color_ramp_node.inputs["Fac"]
+    )
+    # マッピングノードからノイズテクスチャノードへの接続
+    material.node_tree.links.new(
+        mapping_node.outputs["Vector"], noise_texture_node.inputs["Vector"]
+    )
+    # テクスチャ座標ノードからマッピングノードへの接続
+    material.node_tree.links.new(
+        texture_coordinate_node.outputs["Generated"], mapping_node.inputs["Vector"]
+    )
+
+    return color_ramp_node
+
+
+# マテリアルを作成する関数
 def create_material(name):
 
     # 新しいマテリアルを作成する
@@ -41,11 +112,19 @@ def create_material(name):
     principled_bsdf_node.inputs["Metallic"].default_value = 1.0
 
     # マテリアルの粗さの値を設定する
-    principled_bsdf_node.inputs["Roughness"].default_value = random.uniform(0.1, 1.0)
+    # principled_bsdf_node.inputs["Roughness"].default_value = random.uniform(0.1, 1.0)
+
+    color_ramp_node = create_noise_mask(material)
+
+    # カラーランプノードからプリンシプルBSDFの粗さへの接続
+    material.node_tree.links.new(
+        color_ramp_node.outputs["Color"], principled_bsdf_node.inputs["Roughness"]
+    )
 
     return material
 
 
+# シーンにICO 球を追加する関数
 def add_mesh():
 
     # ICO 球を作成する
@@ -60,6 +139,7 @@ def add_mesh():
     return mesh_obj
 
 
+# メイン関数
 def main():
 
     partially_clean_the_scene()
